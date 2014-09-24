1459aa352f9b35caa9a22ef322eb92f5cc78bded
Andy Wong <alacritythief@gmail.com>
2014-09-24 11:21:10 -0400

    fixed tests, and index html

--- spec/features/new_review_spec.rb
+++ spec/features/new_review_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 feature "new review" do
+  before :each do
+    user = User.create!(email: "foo@example.com", password: "123456578")
+
+    visit "/users/sign_in"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Log in"
+  end
+
   scenario "user creates a review" do
     cat = Cat.create(name:"cat", url:"http://www.thinkcontra.com/wp-content/uploads/2013/04/whiskey-and-cats-photo-u1-e1365195706240.jpeg")
     visit cat_path(cat)
---
+++ spec/features/view_cats_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "view cats" do
+  scenario "list cats on index page" do
+    cats = []
+    user = User.create!(email: "foo@example.com", password: "123456578")
+    5.times do |i|
+      cats << Cat.create!(
+        name: "very fancy cat #{i}",
+        description: "fanciest cat I've seen all day",
+        url: "http://example.com/cat_#{i}.png")
+    end
+
+    visit cats_path
+
+    cats.each do |cat|
+      expect(page).to have_content(cat.name)
+      expect(page).to have_css("img[src=\"#{cat.url}\"]")
+    end
+  end
+end
+
+feature "view cat" do
+  scenario "view cat" do
+    cat = Cat.create(name:"cat", url:"http://www.thinkcontra.com/wp-content/uploads/2013/04/whiskey-and-cats-photo-u1-e1365195706240.jpeg")
+
+    visit cat_path(cat)
+      expect(page).to have_content(cat.name)
+      expect(page).to have_css("img[src=\"#{cat.url}\"]")
+  end
+end
